6d0a3c6f3dee9872a64579e6eaace6019c1fd672
Christian Lesmes <clesmesc@gmail.com>
2017-09-08 23:03:35 -0500

    Agregados comentarios y anotaciones faltantes a ProductoEntity

--- src/main/java/co/konrad/project1/ntd/entities/ProductoEntity.java
+++ src/main/java/co/konrad/project1/ntd/entities/ProductoEntity.java
@@ -6,34 +6,79 @@
 package co.konrad.project1.ntd.entities;
 
 import java.io.Serializable;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 /**
  *
  * @author PC
  */
 @Entity
+@Table(name="producto")
 public class ProductoEntity implements Serializable{
+    /**
+     * llave primaria del producto
+     */
     @Id
-    Long productoId;
-    String nombre;
-    String descripcion;
-    String stock;
-    Long precio;
-    String marca;
-    String garantia;
-    Long categoriaId;
-    Long proveedorId;
-    Long promocionId;
-    Long comentariosId;
+    @Column(name="id_producto", unique=true)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    /**
+     * nombre del producto
+     */
+    @Column(name="nombre_producto")
+    private String nombre;
+    /**
+     * descripción del producto
+     */
+    @Column(name="descripcion_producto")
+    private String descripcion;
+    /**
+     * stock del producto
+     */
+    @Column(name="stock_producto")
+    private String stock;
+    /**
+     * precio del producto
+     */
+    @Column(name="precio_producto")
+    private Long precio;
+    /**
+     * marca del producto
+     */
+    @Column(name="marca_producto")
+    private String marca;
+    /**
+     * garantía del producto
+     */
+    @Column(name="garantia_producto")
+    private String garantia;
+    /**
+     * llave foranea del producto hacia categoría
+     */
+    @Column(name="id_categoria")
+    private Long categoriaId;
+    /**
+     * llave foranea del producto hacia proveedor
+     */
+    @Column(name="id_proveedor")
+    private Long proveedorId;
+    /**
+     * llave foranea del producto hacia promoción
+     */
+    @Column(name="id_promocion")
+    private Long promocionId;
+    /**
+     * llave foranea del producto hacia comentarios
+     */
+    @Column(name="id_comentarios")
+    private Long comentariosId;
 
-    public Long getProductoId() {
-        return productoId;
+    public Long getId() {
+        return id;
     }
 
-    public void setProductoId(Long productoId) {
-        this.productoId = productoId;
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getNombre() {
@@ -115,6 +160,4 @@ public class ProductoEntity implements Serializable{
     public void setComentariosId(Long comentariosId) {
         this.comentariosId = comentariosId;
     }
-    
-    
 }
